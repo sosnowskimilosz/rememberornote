59660ef97f77674fdab5436777da826f8b3d7c1d
sosnowskimilosz <sosnowskimilosz@gmail.com>
2022-05-13 15:40:31 +0200

    initial commit with files

---
+++ src/main/java/com/milo/rememberornote/RememberornoteApplication.java
@@ -0,0 +1,13 @@
+package com.milo.rememberornote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberornoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberornoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/milo/rememberornote/RememberornoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milo.rememberornote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberornoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
